2942b9a02644abb12d1ebfa4dae2dc6925daa5a7
Christopher Gjersing <csgjersing@gmail.com>
2022-02-07 14:52:15 -0800

    Update loveCalc.js
    New conditional statements to provide some character to output message

--- js-tests/loveCalc.js
+++ js-tests/loveCalc.js
@@ -4,4 +4,15 @@ function getLove() {
 
 var userName = prompt("What is your name?");
 var crushName = prompt("what is your crush's name?");
-alert(userName + " and " + crushName + " are " + getLove() + "% compatible!");
+var loveScore = getLove();
+
+if (loveScore >90) {
+    alert(userName + " and " + crushName + " love each other like Kanye loves Kanye..." + loveScore + "!");
+} else if (loveScore > 70) {
+    alert(userName + " and " + crushName + " are " + loveScore + "% compatible! Get married!");
+} else if (loveScore > 40) {
+    alert(userName + " and " + crushName + " are " + loveScore + "% compatible!\n Could be better...");
+} else {
+        alert(userName + " and " + crushName + " are " + loveScore + "% compatible!\n Yikes!");
+
+}
